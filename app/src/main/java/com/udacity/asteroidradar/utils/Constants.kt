c8659295c2e4bed78414e9bff2c84b7208a57d5e
EmilioHoffmann <emiliohoffmann@hotmail.com>
2021-11-27 17:20:43 -0300

    Moving files to new packages; Adding API KEY

--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -1,7 +1,8 @@
-package com.udacity.asteroidradar
+package com.udacity.asteroidradar.utils
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
+    const val NASA_API_KEY = "u4qmNpzXXZBmfw3ojRpgul5qnVeiz4tpsF0HdF9D"
 }
